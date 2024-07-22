8107fa36983fe1e35a10c765562ba6e0e1f70375
Mattias-08 Jörntell <mattias.jorntell@gmail.com>
2024-07-22 20:30:32 +0000

    adjust views

--- env.py
+++ env.py
@@ -1,6 +1,6 @@
 import os
 
 os.environ.setdefault(
-    "DATABASE_URL", "postgres://uztioasxwx8:k3YgEd6HkBpj@ep-gentle-mountain-a23bxz6h.eu-central-1.aws.neon.tech/scan_tribe_dug_75950"
+    "DATABASE_URL", "postgres://uztioasxwx8:k3YgEd6HkBpj@ep-gentle-mountain-a23bxz6h.eu-central-1.aws.neon.tech/shed_cross_photo_614695"
 )
 os.environ.setdefault("SECRET_KEY", "test")
