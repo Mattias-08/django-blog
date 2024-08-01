12db2a06df8b5efc5961503f1b5c8e0d67ccbaa9
Mattias Jörntell <mattias.jorntell@gmail.com>
2024-08-01 08:36:44 +0000

    adjustments

--- env.py
+++ env.py
@@ -3,4 +3,6 @@ import os
 os.environ.setdefault(
     "DATABASE_URL", "postgres://uztioasxwx8:k3YgEd6HkBpj@ep-gentle-mountain-a23bxz6h.eu-central-1.aws.neon.tech/shed_cross_photo_614695")
 os.environ.setdefault(
-    "SECRET_KEY", "cloudinary://957129678784598:vWm2WBKCIS0NYWXwjRrfDMtbMZc@dfi57wuit")
+    "SECRET_KEY", "test")
+os.environ.setdefault(
+   "CLOUDINARY_URL", "cloudinary://957129678784598:vWm2WBKCIS0NYWXwjRrfDMtbMZc@dfi57wuit")
